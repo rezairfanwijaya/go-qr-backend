6dd1c51426b549a20c3afcb79fe8dfa1c28242f9
Reza Irfan Wijaya <rezaaagit@gmail.com>
2023-01-01 12:45:41 +0700

    add: handler generate qr

---
+++ handler/qr.go
@@ -0,0 +1,52 @@
+package handler
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/rezairfanwijaya/go-qr-backend/qr"
+	"github.com/rezairfanwijaya/go-qr-backend/utils"
+)
+
+type QRHandler struct {
+	qrService qr.IQRService
+}
+
+// new handler
+func NewQRHandler(qrService qr.IQRService) *QRHandler {
+	return &QRHandler{qrService}
+}
+
+// implementation
+func (h *QRHandler) GenerateQR(c *gin.Context) {
+	var input qr.InputBodyQR
+
+	// binding
+	if err := c.ShouldBindJSON(&input); err != nil {
+		response := utils.ResponseAPI(
+			"failed",
+			err.Error(),
+			http.StatusBadRequest,
+			nil,
+		)
+
+		c.JSON(http.StatusBadRequest, response)
+		return
+	}
+
+	// call service
+	qrCode, err := h.qrService.GenerateQR(input.Input)
+	if err != nil {
+		response := utils.ResponseAPI(
+			"failed",
+			err.Error(),
+			http.StatusBadRequest,
+			nil,
+		)
+
+		c.JSON(http.StatusBadRequest, response)
+		return
+	}
+
+	c.Data(http.StatusOK, "", qrCode)
+}
