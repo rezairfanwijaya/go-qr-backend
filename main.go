30583832ebe2e37ab3a29763d337f40471c81903
Reza Irfan Wijaya <rezaaagit@gmail.com>
2023-01-01 12:47:35 +0700

    add: endpoint generate qr

--- main.go
+++ main.go
@@ -1,5 +1,27 @@
 package main
 
+import (
+	"log"
+
+	"github.com/gin-gonic/gin"
+	"github.com/rezairfanwijaya/go-qr-backend/handler"
+	"github.com/rezairfanwijaya/go-qr-backend/qr"
+)
+
 func main() {
-	println("Hello World")
+	// init gin
+	r := gin.Default()
+
+	// init service qr
+	service := qr.NewQRService()
+	// init handler qr
+	handler := handler.NewQRHandler(service)
+
+	// endpoint
+	r.POST("generate", handler.GenerateQR)
+
+	// run server
+	if err := r.Run(":8989"); err != nil {
+		log.Fatal(err)
+	}
 }
