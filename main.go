107c7b2c6e3ee5a24dea40a68d869312650240cd
Reza Irfan Wijaya <rezaaagit@gmail.com>
2023-01-02 00:31:22 +0700

    add: allow cors

--- main.go
+++ main.go
@@ -11,6 +11,7 @@ import (
 func main() {
 	// init gin
 	r := gin.Default()
+	r.Use(corsMiddleware())
 
 	// init service qr
 	service := qr.NewQRService()
@@ -23,5 +24,21 @@ func main() {
 	// run server
 	if err := r.Run(":8989"); err != nil {
 		log.Fatal(err)
+	}
+}
+
+func corsMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Method", "*")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(200)
+			return
+		}
+
+		c.Next()
 	}
 }
